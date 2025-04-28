fe1ed4cbfb885a51506f89d59f02870f1ae06944
Mark Luis <markluisbertillo11@gmail.com>
2025-04-28 20:35:54 +0800

    index

--- Homepage.php
+++ Homepage.php
@@ -3,7 +3,7 @@ session_start();
 
 // Redirect to login if not authenticated
 if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
+  header("Location: index.php");
     exit();
 }
 
--- send-password.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$email = $_POST['email'] ??;
