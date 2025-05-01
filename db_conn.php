2d91ea7f4f8fe39e51293a104b1e2e741459755c
Mark Luis <markluisbertillo11@gmail.com>
2025-05-01 13:00:28 +0800

    dbconn

--- db_conn.php
+++ db_conn.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'localhost';
-$dbname = 'nursingthoughts';
-$username = 'root';
-$password = ''; // your actual password
+
+$servername = 'srv1786.hstgr.io';
+$dbname = 'u572625467_thoughts';
+$username = 'u572625467_thoughts';
+$password = ':5kkw#nI'; // your actual password
 
 try {
      // Create a new PDO instance
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode to exceptions
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());  // Handle connection errors
